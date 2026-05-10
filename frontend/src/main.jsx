4e200bd52ce0b4e32847f96956e9780e4ac06404
tech tech <tech-imp@anepgroupe.local>
2026-05-10 15:14:52 +0100

    Update Google Client ID

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // ID Client Google (mettre le vrai pour la prod)
-const GOOGLE_CLIENT_ID = '605173498248-3biu1juoa2r7eiurgd14vduvol660r16.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '515406147791-11e503qt6e61lq1bphbk2hlo37c4gogo.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
